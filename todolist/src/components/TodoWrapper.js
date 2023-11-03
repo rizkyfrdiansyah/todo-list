39cfd914ec080429df4a4d576555b31e744259ae
Muhammad Rizky Ferdiansyah <muhammadrizkyferdiansyah@gmail.com>
2023-11-03 22:52:27 +0700

    update TodoForm:handleSubmit

--- todolist/src/components/TodoWrapper.js
+++ todolist/src/components/TodoWrapper.js
@@ -1,5 +1,10 @@
 import React from "react";
+import { TodoForm } from "./TodoForm";
 
 export const TodoWrapper = () => {
-  return <div>TodoWrapper</div>;
+  return (
+    <div className="TodoWrapper">
+      <TodoForm />
+    </div>
+  );
 };
