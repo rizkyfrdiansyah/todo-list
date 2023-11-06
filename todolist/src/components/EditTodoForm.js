05e9fc138a1b4556aa5b14e75e6806c8482d26c2
Muhammad Rizky Ferdiansyah <muhammadrizkyferdiansyah@gmail.com>
2023-11-06 23:48:10 +0700

    bug fixing/done

---
+++ todolist/src/components/EditTodoForm.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+
+export const EditTodoForm = ({ editTodo, task }) => {
+  const [value, setValue] = useState(task.task);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    editTodo(value, task.id);
+
+    setValue("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="TodoForm">
+      <input type="text" className="todo-input" value={value} placeholder="Update Task" onChange={(e) => setValue(e.target.value)} />
+      <button type="submit" className="todo-btn">
+        Update Task
+      </button>
+    </form>
+  );
+};
--- todolist/src/components/TodoForm.js
+++ todolist/src/components/TodoForm.js
@@ -1,18 +1,19 @@
 import React, { useState } from "react";
 
-export const TodoForm = () => {
-  // TODO: Melakukan handleSubmit
+export const TodoForm = ({ addTodo }) => {
   const [value, setValue] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    console.log(value);
+    addTodo(value);
+
+    setValue("");
   };
 
   return (
     <form className="TodoForm" onSubmit={handleSubmit}>
-      <input type="text" className="todo-input" placeholder="Apa yang ingin kamu lakukan?" onChange={(e) => setValue(e.target.value)} />
+      <input type="text" className="todo-input" value={value} placeholder="Apa yang ingin kamu lakukan?" onChange={(e) => setValue(e.target.value)} />
       <button type="submit" className="todo-btn">
         Add Task
       </button>
